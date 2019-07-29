7c571c60f70602ba8aa52e9709a28c58f99173e0
Belousov Paul <belousov.workmail@gmail.com>
2019-07-29 23:37:53 +0300

    init

--- app/src/main/java/com/example/locksound/MainActivity.java
+++ app/src/main/java/com/example/locksound/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.locksound;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,12 +24,23 @@ public class MainActivity extends AppCompatActivity {
 
     AudioManager audioManager;
 
+    private BroadcastReceiver mReceiver;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mReceiver = new ScreenReciever();
+
+        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        intentFilter.addAction(Intent.ACTION_USER_PRESENT);
+        intentFilter.setPriority(999);
+
+
+        registerReceiver(mReceiver, intentFilter);
 
 
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
---
+++ app/src/main/java/com/example/locksound/ScreenReciever.java
@@ -0,0 +1,22 @@
+package com.example.locksound;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ScreenReciever extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if(intent.getAction().equals(Intent.ACTION_SCREEN_OFF)){
+            Log.e("SCREEEN", "Screen was locked!");
+
+        } else if(intent.getAction().equals(Intent.ACTION_USER_PRESENT)){
+            Log.e("SCREEN", "User unlock the phone!");
+        }
+        }
+
+    }
+
